d7d3ff78083530ba0c398241a3e36356de6e6384
aziz_ <aziz_korkmaz21@hotmail.com>
2019-07-01 23:40:03 +0300

    controller view yapıları

--- app/Http/Controllers/AnasayfaController.php
+++ app/Http/Controllers/AnasayfaController.php
@@ -6,7 +6,20 @@ use Illuminate\Http\Request;
 
 class AnasayfaController extends Controller
 {
+    /*//view yönlendirme
     public function index(){
         return view("anasayfa");
+    }*/
+
+    /*//parametre gönderme
+    public function index(){
+        return view("anasayfa" , ["isim" => "Aziz"]);
+    }*/
+
+    //parametre gönderme
+    public function index(){
+        $isim = "Aziz";
+        $soyisim = "Korkmaz";
+        return view("anasayfa", compact("isim", " soyisim"));
     }
 }
