7db3eab7eacd9884b82896316b7a9711a34ba00b
aziz_ <aziz_korkmaz21@hotmail.com>
2019-07-01 17:37:52 +0300

    route işlemleri

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//sitring değer döndürme
+Route::get("/merhaba", function(){
+    return "Merhaba";
+});
+
+//json değerini döndürme --> dizi ile döndüre biliriz.
+Route::get("/api/v1/merhaba", function (){
+    return ["mesaj" => "Merhaba"];
+});
+
+//route ile parametre almak
+Route::get("/urun/{urun_adi}", function($urun_adi){
+    return "Ürün adı : $urun_adi";
+});
+
+//route a 2 parametre gnderme ve opsiyonel parametre göndeme
+Route::get("/urun/{urun_adi}/{id?}", function($urun_adi, $id=0){
+    return "Ürün adı: $id $urun_adi";
+}) ->name("urun_detay");//burada isim verebiliriz
+
+//route ta yönlendirme işlemi
+Route::get("/kampanya", function(){
+   return redirect()->route("urun_detay", ["id" =>7, "urun_adi" => "Portakal"]);//route parametrelerini dizi ile gönderiyoruz
+});
